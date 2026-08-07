50fa334e5972c8650b7b7985097aedff9142e735
ma-all <marwaalbaloushi21@gmail.com>
2026-08-07 16:13:53 +0300

    created the menu item model

---
+++ models/menuItem.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const menuItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        enum: ['Coffee', 'Non-Coffee', 'Pastry'],
+    },
+    caffeine: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true,
+        min: 0.1,
+    },
+}, {timestamps: true})
+
+const MenuItem = mongoose.model('MenuItem', menuItemSchema)
+
+module.exports = MenuItem
